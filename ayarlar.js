5c00b3676dcaef9888c9f9a56087e67b7ce16bc1
Lawnless <69431018+Lawnless@users.noreply.github.com>
2021-05-16 11:36:32 +0300

    Create ayarlar.js

---
+++ ayarlar.js
@@ -0,0 +1,5 @@
+module.exports = {
+	"botPrefix": "!", // Botun ön-ekini girin, varsayılan değer: "!"
+	"botToken": "ODQzNDA0OTU3ODMwMDg2NjU3.YKDYEw.9ZfSZvoT-VO5eKwiQuF8wetcubY", // Botun tokenini girin.
+	"botSahip": "589104745898049546", // Bot sahibinin idsini girin.
+};
